e14a1e341efa744dd1d8fae33c7f3c742d2e30c6
KASAI Masatoshi <kassailattice628@gmail.com>
2023-01-31 19:05:59 +0900

    Correct var of video capture duration

--- DAQ/Video/CheckDuration_Camera.m
+++ DAQ/Video/CheckDuration_Camera.m
@@ -1,15 +1,14 @@
 function CheckDuration_Camera(app)
 
 rect = app.recobj.rect;
-if app.CaptureTime.Value > rect
+
+if app.VideoCaptureTime.Value > rect
     errordlg('Camera capture duration is longer than the DAQ recording time!')
     
     %Reset capture time
-    app.CaptureTime.Value = 1000;
-    app.imaq.duration_ms = 1000;
-else
-    app.imaq.duration_ms = app.VideoCaptureTime.Value;
+    app.VideoCaptureTime.Value = 1000;
 end
 
+app.imaq.duration_ms = app.VideoCaptureTime.Value;
 
 end
