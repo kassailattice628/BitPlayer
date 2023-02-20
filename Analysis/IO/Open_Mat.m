b6c08eadb731d429dd680f397b4ff63a85827061
KASAI Masatoshi <kassailattice628@gmailcom>
2023-02-20 10:50:09 +0900

    File load function.

--- Analysis/IO/Open_Mat.m
+++ Analysis/IO/Open_Mat.m
@@ -5,76 +5,87 @@ function Open_Mat(app)
 % 2023/02/10
 % Select [DAQ].mat file and extract each data.
 
-%% Clean up variables.
+%% Clean up preloaded variables;
 
-app.DataSave = [];
-app.imgobj = [];
-app.sobj = [];
-app.recobj = [];
-app.ParamsSave = [];
-DataSave = [];
-app.n = 1;
+% app.SaveData= [];
+% app.SaveTimestamps
+% app.imgobj = [];
+% app.sobj = [];
+% app.recobj = [];
+% app.ParamsSave = [];
+
+SaveData = [];
+app.n_in_loop = 1;
 
 %% Load New Data
 if isempty(app.mainvar)
-    % No information about save data directry.
+    % If this is the first time to select daq data.
+    % There is no information about location of the ,mat files.
     [f, d] = uigetfile({'~/Share/s2p_working/*.mat'});
-    load([d,f]); %#ok<*LOAD
     
-    while isempty(DataSave)
-        errordlg('DataSave is missing! Select other file.')
-        % select another file
-        [f, d] = uigetfile({[d, '*.mat']});
-        load([d, f]);
+    if f == 0 
+        % When file is not selected.
+        errordlg('File is not selected.')
+    else
+        load([d,f]); %#ok<*LOAD
+        while isempty(SaveData)
+            errordlg('DataSave is missing! Select other file.')
+            % Select another file
+            [f, d] = uigetfile({[d, '*.mat']});
+            load([d, f]);
+        end
+        
+        %% Assign vars of savedata directory
+        mainvar.dirname = d;
+        
+        %% Check file name
+        if length(regexp(f, '_')) > 1
+            i = regexp(f, '_');
+            mainvar.fname = [f(1:i(end)-1), '.mat'];
+            disp(f)
+        else
+            mainvar.fname = f;
+        end
+        
+        app.FileName.Text = ['File Name: ', f];
+        app.SaveFileName.Value = f;
+        
+        %% Check imgobj <- for imaging data
+        if ~exist('imgobj', 'var')
+            %First time to select mat file.
+            imgobj.nROI = 0;
+            imgobj.selectROI = 1;
+            imgobj.maxROIs =  0;
+            imgobj.dFF =[];
+            
+            %Photo sensor V -> mV
+            SaveData(:,3,:) =  SaveData(:,3,:)* 1000;
+            
+%         elseif ~isfield(imgobj, 'dFF')  %#ok<NODEF>
+%             imgobj.dFF = [];
+        end
+        
+        %% Check frame rate
+        if isfield(imgobj, 'FVsampt')
+            %Put in GUI.
+            app.FVsampt.Value = imgobj.FVsampt;
+        end
+        
+        %         app.filenameLabel.Text = f;
+        %         app.StimPatternLabel.Text = sobj.pattern;
+        
+        %% Return
+        app.imgobj = imgobj;
+        app.mainvar = mainvar;
+%         app.ParamsSave = ParamsSave;
+        app.SaveData = SaveData;
+        app.recobj = recobj;
+%         app.sobj = sobj;
+        
+        disp(mainvar)
+        
     end
-    
-else
-    %Select New Data
-    [f, d] = uigetfile({[app.mainvar.dirname, '*.mat']});
-    load([d,f]);
 end
 
-%% Assign vars of savedata directory and file name
-mainvar.dirname = d;
-mainvar.fname = f;
 
-%% Check file name
-if length(regexp(f, '_')) > 1
-    i = regexp(f, '_');
-    mainvar.fname = [f(1:i(end)-1), '.mat'];
-    disp(f)
-else
-    mainvar.fname = f;
-end
-
-%% Check imgobj <- for imaging data
-if ~exist('imgobj', 'var')
-    imgobj.nROI = 0;
-    imgobj.selectROI = 1;
-    imgobj.maxROIs =  0;
-    imgobj.dFF =[];
-    imgobj.FVsampt_tentative = 0.574616;
-    DataSave(:,3,:) =  DataSave(:,3,:)* 1000;
-    
-elseif ~isfield(imgobj, 'dFF')  %#ok<NODEF>
-    imgobj.dFF = [];
-end
-
-if isfield(imgobj, 'FVsampt')
-    %Put in GUI.
-    app.FVsampt.Value = imgobj.FVsampt;
-end
-
-app.filenameLabel.Text = f;
-app.StimPatternLabel.Text = sobj.pattern;
-
-%% output
-app.imgobj = imgobj;
-app.mainvar = mainvar;
-app.ParamsSave = ParamsSave;
-app.DataSave = DataSave;
-app.recobj = recobj;
-app.sobj = sobj;
-
-disp(mainvar)
 end
